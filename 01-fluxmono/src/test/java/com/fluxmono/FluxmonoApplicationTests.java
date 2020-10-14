75d7e90d268899335ecd56795c8a60c7a4d52530
Archie Isdiningrat <archie.isdiningrat@tiket.com>
2020-10-14 20:05:12 +0700

    init project

---
+++ 01-fluxmono/src/test/java/com/fluxmono/FluxmonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxmonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
